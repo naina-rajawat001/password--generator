97849ab321d12012c3cbc6f13debf3c9f9727685
naina-rajawat001 <naina.rajawat001@gmail.com>
2025-09-25 10:50:36 +0530

    commit

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let  inputEl = document.getElementById("length-int");
 
 
 
-let password =  ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+let characters =  ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
 let mode = "dark";
 
@@ -40,6 +40,7 @@ generateBtn.onclick = () => {
 
     let randomNum1 = "";
     let randomNum2 = ""; 
+
     for (let i = 0 ; i < length; i++) {
         randomNum1 += password[Math.floor(Math.random() * password.length)]
         randomNum2 += password[Math.floor(Math.random() * password.length)]
